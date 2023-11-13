9c8ef4070f61e01777806a6ec6670fbf54217d45
jtlandis <jtlandis314@gmail.com>
2023-11-13 10:41:44 -0500

    making module more safe to call - will notify user where to get .gtf file used for project

--- mods/gr.r
+++ mods/gr.r
@@ -1,13 +1,18 @@
 
 
 box::use(
-  ./gtf
+  ./gtf,
+  rlang[abort]
 )
 
 expected_gencode_file <- "gencode.v44.annotation.gtf"
 gencode_file <- box::file(expected_gencode_file)
 if (!file.exists(gencode_file))
-  stop(paste0('file: `',expected_gencode_file,'` was not found in path `', box::file(), '`'), call. = F)
+  abort(
+    c(sprintf("file: `%s` was not found in path `%s`", expected_gencode_file, box::file()),
+      "i" = "module `gr` requires a gtf file reference which likely hasnt been downloaded or is not named correctly.",
+      "i" = "download from: 'https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_44/gencode.v44.annotation.gtf.gz'"))
+
 
 if (!file.exists(rds_file <- box::file('genomicRanges.rds'))) {
   GR <- gtf$read_gtf2GR(gencode_file)
